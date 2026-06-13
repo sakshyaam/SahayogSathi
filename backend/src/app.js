d2d0c595daf03082ef948428efcd7293a4dd53ea
sakshyam <sakshyamdhakal01@gmail.com>
2026-06-13 17:25:52 +0545

    learning real time messaging

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,8 @@
 import express from "express";
 import cors from "cors"
 import cookieParser from "cookie-parser";
+import { app } from "./socket/socket.js";
 
-
-const app = express();
 
 app.use(cors({
     origin : process.env.CORS_ORIGIN,
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
 import connectDB from "./db/DbConnect.js";
-import app from "./app.js";
+import { server } from "./socket/socket.js";
 
 dotenv.config();
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    server.listen(process.env.PORT || 8000, () => {
       console.log(`Server is listening : ${process.env.PORT}`);
     });
   })
